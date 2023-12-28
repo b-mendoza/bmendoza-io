cec60130c4ed2918cebe48d637c0297f9d3f111a
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-28 02:46:43 -0600

    feat(tailwind): add Tailwind CSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
