b0d721e7c2d4a1c24e151cf71e470fda5dc6b959
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-28 16:44:24 -0600

    feat(models): add getTags model

---
+++ src/models/get-tags.model.ts
@@ -0,0 +1,48 @@
+/* import type { IconName } from '~/components/icon'; */
+
+export type Tag = {
+  text: string;
+  /* icon?: IconName; */
+};
+
+export const getTags = (): Tag[] => {
+  /* This might be replaced by a database/CMS call in the future */
+  return [
+    {
+      text: 'JavaScript',
+      /* icon: 'javascript', */
+    },
+    {
+      text: 'TypeScript',
+      /* icon: 'typescript', */
+    },
+    {
+      text: 'React',
+      /* icon: 'react', */
+    },
+    {
+      text: 'Remix',
+      /* icon: 'remix', */
+    },
+    {
+      text: 'Next.js',
+      /* icon: 'nextdotjs', */
+    },
+    {
+      text: 'Tailwind CSS',
+      /* icon: 'tailwindcss', */
+    },
+    {
+      text: 'Node.js',
+      /* icon: 'nodedotjs', */
+    },
+    {
+      text: 'GraphQL',
+      /* icon: 'graphql', */
+    },
+    {
+      text: 'PostgreSQL',
+      /* icon: 'postgresql', */
+    },
+  ];
+};
